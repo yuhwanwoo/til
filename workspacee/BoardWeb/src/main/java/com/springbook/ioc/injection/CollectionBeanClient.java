bb2a07fd54ac2c0fb0f24d0853eda3baad3ced92
yuhwanwoo <6514687@naver.com>
2020-03-02 15:39:07 +0900

    Add files via upload

---
+++ workspacee/BoardWeb/src/main/java/com/springbook/ioc/injection/CollectionBeanClient.java
@@ -0,0 +1,23 @@
+package com.springbook.ioc.injection;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class CollectionBeanClient {
+
+	public static void main(String[] args) {
+		AbstractApplicationContext factory=new GenericXmlApplicationContext("applicationContext.xml");
+		
+		CollectionBean bean =(CollectionBean)factory.getBean("collectionBean");
+		Map<String,String> addressList = bean.getAddressList();
+		/*for(String address:addressList) {
+			System.out.println(address.toString());
+		}*/
+		factory.close();
+	}
+
+}
